ccd673d6a3996b88f9fa10c19d878b07949d7935
rraavviii <ravikumar16208062@gmail.com>
2025-02-13 05:00:20 +0530

    Initial commit for frontend

---
+++ src/App.jsx
@@ -0,0 +1,64 @@
+import { useState, useEffect } from 'react'
+import "prismjs/themes/prism-tomorrow.css"
+import Editor from "react-simple-code-editor"
+import prism from "prismjs"
+import Markdown from "react-markdown"
+import rehypeHighlight from "rehype-highlight";
+import "highlight.js/styles/github-dark.css";
+import axios from 'axios'
+import './App.css'
+
+function App() {
+  const [code, setCode] = useState(` function sum() {
+  return 1 + 1
+}`);
+  const [review, setReview] = useState(``);
+  const [buttonText, setButtonText] = useState("Review");
+
+  useEffect(() => {
+    prism.highlightAll();
+  }, []);
+
+  async function reviewCode() {
+    setButtonText("Reviewing..."); 
+    try {
+      const response = await axios.post("http://localhost:3000/ai/get-review", { code });
+      // const response = await axios.post('https://your-backend-url.onrender.com/ai/get-review', { code });
+      setReview(response.data);
+    } catch (error) {
+      setReview("Error fetching review.");
+    }
+    setButtonText("Review");
+  }
+
+  return (
+    <>
+      <main>
+        <div className="left">
+          <div className="code">
+            <Editor
+              value={code}
+              onValueChange={code => setCode(code)}
+              highlight={code => prism.highlight(code, prism.languages.javascript, "javascript")}
+              padding={10}
+              style={{
+                fontFamily: '"Fira code", "Fira Mono", monospace',
+                fontSize: 16,
+                border: "1px solid #ddd",
+                borderRadius: "5px",
+                height: "100%",
+                width: "100%"
+              }}
+            />
+          </div>
+          <div onClick={reviewCode} className="review">{buttonText}</div>
+        </div>
+        <div className="right">
+          <Markdown rehypePlugins={[rehypeHighlight]}>{review}</Markdown>
+        </div>
+      </main>
+    </>
+  );
+}
+
+export default App;
